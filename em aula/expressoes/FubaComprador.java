0f741aa1cd9451ee55d9796826a2cc658cda53d5
Manoela Macchion Martedi <ra00331711@pucsp.edu.br>
2024-11-05 08:32:17 -0300

    aula 05/11

--- em aula/expressoes/FubaComprador.java
+++ em aula/expressoes/FubaComprador.java
@@ -1,14 +1,25 @@
-
 class FubaComprador
 {
-    void montarTabela(double preco, int meses, double taxa) {
-        System.out.println("|| Mes || Juros || Prestacao || Saldo ||\n");
-        for (int j = 1; j <= meses; j++) {
-            System.out.println("[ " + j + " ]");
-            
+    void montarTabela(double preco, int parcelas, double taxa) {
+        double saldoDevedor, prestacao, precoFinal, juros;
+        saldoDevedor = preco;
+        prestacao = pm1(preco, taxa, parcelas);
+        precoFinal = prestacao * parcelas;
+
+        System.out.printf("Valor da prestação mensal: R$ %.2f\n", prestacao);
+        System.out.printf("Preço final do notebook: R$ %.2f\n\n", precoFinal);
+        
+        System.out.printf("%-5s | %-10s | %-10s | %-10s\n", "Mês", "Juros", "Prestação", "Saldo");
+        System.out.println("-------------------------------------------------------");
+        
+        for (int j = 1; j <= parcelas; j++) {
+            juros = saldoDevedor * taxa;
+            saldoDevedor -= (prestacao - juros);
+
+            System.out.printf("%-5d | %-10.2f | %-10.2f | %-10.2f\n", j, juros, prestacao, saldoDevedor);
         }
     }
-    int calcularParcela (double PV, double i, int n) {
-        return (PV*i) / (1 - Math.pow(1+i, -n));
+    double pm1 (double v, double i, int n) {
+        return v * (Math.pow(1 + i, n) * i) / (Math.pow(1 + i, n) - 1);
     }
 }
